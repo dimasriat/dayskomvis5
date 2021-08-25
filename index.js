bcc776e679e86bca064f677dade94be1092ecc93
Dimas Riatmodjo <dimasriat@gmail.com>
2021-08-25 16:54:03 +0700

    buku taum udah jadi

--- index.js
+++ index.js
@@ -5,8 +5,17 @@ const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 app.use("/assets", express.static(path.join(__dirname, "public")));
+
 app.get("/", (req, res) => {
-	return res.render("index", { pageName: "/" });
+	return res.render("buku_tamu");
+});
+
+app.post("/", (req, res) => {
+	return res.redirect('/home');
+});
+
+app.get("/home", (req, res) => {
+	return res.render("index", { pageName: "/home" });
 });
 app.get("/tentang", (req, res) => {
 	return res.render("tentang", { pageName: "/tentang" });
