9d7a59bdedc3d4403ea0d4a37d8e01cb6c833acb
Dimas Riatmodjo <dimasriat@gmail.com>
2021-09-01 02:35:02 +0700

    menambahkan karya 3

--- routes/praEventRouter.js
+++ routes/praEventRouter.js
@@ -26,9 +26,19 @@ router.get("/", (req, res) => {
 	const pra_event = shuffle(api.pra_event);
 	return res.render("pra_event", { pageName: "/pra-event", pra_event });
 });
-router.get("/:id", (req, res) => {
-	return res.render(`pra_event/pra_event_${req.params.id}`, { pageName: "/pra-event" });
+router.get("/1", (req, res) => {
+	const id = 1;
+	return res.render(`pra_event/pra_event_${id}`, {
+		pageName: "/pra-event",
+		karya: api.pra_event[id - 1],
+	});
 });
-
+router.get("/3", (req, res) => {
+	const id = 3;
+	return res.render(`pra_event/pra_event_${id}`, {
+		pageName: "/pra-event",
+		karya: api.pra_event[id - 1],
+	});
+});
 
 module.exports = router;
